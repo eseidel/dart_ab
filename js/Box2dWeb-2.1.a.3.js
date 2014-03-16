8e769a1ca0a90e52825dff66eac74fa654d7ec34
Alex Russell <slightlyoff@chromium.org>
2014-03-15 18:17:47 -0700

    More math method cleanup.

--- js/Box2dWeb-2.1.a.3.js
+++ js/Box2dWeb-2.1.a.3.js
@@ -2212,12 +2212,6 @@ function b2Mat33() {
 Box2D.Common.Math.b2Mat33 = b2Mat33;
 
 // TODO(slightlyoff): inherit_()
-function b2Math() {
-   b2Math.b2Math.apply(this, arguments);
-};
-Box2D.Common.Math.b2Math = b2Math;
-
-// TODO(slightlyoff): inherit_()
 function b2Sweep() {
    b2Sweep.b2Sweep.apply(this, arguments);
 };
@@ -3585,18 +3579,6 @@ Box2D.postDefs.push(function () {
   Box2D.Collision.Shapes.b2Shape.e_missCollide = 0;
   Box2D.Collision.Shapes.b2Shape.e_startsInsideCollide = parseInt((-1));
 });
-// })();
-// (function () {
-var b2Color = Box2D.Common.b2Color,
-  b2internal = Box2D.Common.b2internal,
-  b2Settings = Box2D.Common.b2Settings,
-  b2Mat22 = Box2D.Common.Math.b2Mat22,
-  b2Mat33 = Box2D.Common.Math.b2Mat33,
-  b2Math = Box2D.Common.Math.b2Math,
-  b2Sweep = Box2D.Common.Math.b2Sweep,
-  b2Transform = Box2D.Common.Math.b2Transform,
-  b2Vec2 = Box2D.Common.Math.b2Vec2,
-  b2Vec3 = Box2D.Common.Math.b2Vec3;
 
 b2Color.b2Color = function () {
   this._r = 0;
@@ -3692,19 +3674,6 @@ Box2D.postDefs.push(function () {
   b2Settings.b2_linearSleepTolerance = 0.01;
   b2Settings.b2_angularSleepTolerance = 2.0 / 180 * b2Settings.b2_pi;
 });
-// })();
-// (function () {
-var b2AABB = Box2D.Collision.b2AABB,
-  b2Color = Box2D.Common.b2Color,
-  b2internal = Box2D.Common.b2internal,
-  b2Settings = Box2D.Common.b2Settings,
-  b2Mat22 = Box2D.Common.Math.b2Mat22,
-  b2Mat33 = Box2D.Common.Math.b2Mat33,
-  b2Math = Box2D.Common.Math.b2Math,
-  b2Sweep = Box2D.Common.Math.b2Sweep,
-  b2Transform = Box2D.Common.Math.b2Transform,
-  b2Vec2 = Box2D.Common.Math.b2Vec2,
-  b2Vec3 = Box2D.Common.Math.b2Vec3;
 
 b2Mat22.b2Mat22 = function () {
   this.col1 = new b2Vec2();
@@ -3899,134 +3868,107 @@ b2Mat33.prototype.Solve33 = function (out, bX, bY, bZ) {
   out.z = det * (a11 * (a22 * bZ - a32 * bY) + a21 * (a32 * bX - a12 * bZ) + a31 * (a12 * bY - a22 * bX));
   return out;
 }
-b2Math.b2Math = function () {};
-b2Math.Dot = function (a, b) {
-  return a.x * b.x + a.y * b.y;
-}
-b2Math.CrossVV = function (a, b) {
-  return a.x * b.y - a.y * b.x;
-}
-b2Math.CrossVF = function (a, s) {
-  var v = new b2Vec2(s * a.y, (-s * a.x));
-  return v;
-}
-b2Math.CrossFV = function (s, a) {
-  var v = new b2Vec2((-s * a.y), s * a.x);
-  return v;
-}
-b2Math.MulMV = function (A, v) {
-  var u = new b2Vec2(A.col1.x * v.x + A.col2.x * v.y,
-                     A.col1.y * v.x + A.col2.y * v.y);
-  return u;
-}
-b2Math.MulTMV = function (A, v) {
-  var u = new b2Vec2(b2Math.Dot(v, A.col1), b2Math.Dot(v, A.col2));
-  return u;
-}
-b2Math.MulX = function (T, v) {
-  var a = b2Math.MulMV(T.R, v);
-  a.x += T.position.x;
-  a.y += T.position.y;
-  return a;
-}
-b2Math.MulXT = function (T, v) {
-  var a = b2Math.SubtractVV(v, T.position);
-  var tX = (a.x * T.R.col1.x + a.y * T.R.col1.y);
-  a.y = (a.x * T.R.col2.x + a.y * T.R.col2.y);
-  a.x = tX;
-  return a;
-}
-b2Math.AddVV = function (a, b) {
-  var v = new b2Vec2(a.x + b.x, a.y + b.y);
-  return v;
-}
-b2Math.SubtractVV = function (a, b) {
-  var v = new b2Vec2(a.x - b.x, a.y - b.y);
-  return v;
-}
-b2Math.Distance = function (a, b) {
-  var cX = a.x - b.x;
-  var cY = a.y - b.y;
-  return Math.sqrt(cX * cX + cY * cY);
-}
-b2Math.DistanceSquared = function (a, b) {
-  var cX = a.x - b.x;
-  var cY = a.y - b.y;
-  return (cX * cX + cY * cY);
-}
-b2Math.MulFV = function (s, a) {
-  if (s === undefined) s = 0;
-  var v = new b2Vec2(s * a.x, s * a.y);
-  return v;
-}
-b2Math.AddMM = function (A, B) {
-  var C = b2Mat22.FromVV(b2Math.AddVV(A.col1, B.col1), b2Math.AddVV(A.col2, B.col2));
-  return C;
-}
-b2Math.MulMM = function (A, B) {
-  var C = b2Mat22.FromVV(b2Math.MulMV(A, B.col1), b2Math.MulMV(A, B.col2));
-  return C;
-}
-b2Math.MulTMM = function (A, B) {
-  var c1 = new b2Vec2(b2Math.Dot(A.col1, B.col1), b2Math.Dot(A.col2, B.col1));
-  var c2 = new b2Vec2(b2Math.Dot(A.col1, B.col2), b2Math.Dot(A.col2, B.col2));
-  var C = b2Mat22.FromVV(c1, c2);
-  return C;
-}
-b2Math.AbsV = function (a) {
-  var b = new b2Vec2(Math.abs(a.x), Math.abs(a.y));
-  return b;
-}
-b2Math.AbsM = function (A) {
-  var B = b2Mat22.FromVV(b2Math.AbsV(A.col1), b2Math.AbsV(A.col2));
-  return B;
-}
-b2Math.MinV = function (a, b) {
-  var c = new b2Vec2(Math.min(a.x, b.x), Math.min(a.y, b.y));
-  return c;
-}
-b2Math.MaxV = function (a, b) {
-  var c = new b2Vec2(Math.max(a.x, b.x), Math.max(a.y, b.y));
-  return c;
-}
-b2Math.Clamp = function (a, low, high) {
-  if (a === undefined) a = 0;
-  if (low === undefined) low = 0;
-  if (high === undefined) high = 0;
-  return a < low ? low : a > high ? high : a;
-}
-b2Math.ClampV = function (a, low, high) {
-  return b2Math.MaxV(low, b2Math.MinV(a, high));
-}
-b2Math.Swap = function (a, b) {
-  var tmp = a[0];
-  a[0] = b[0];
-  b[0] = tmp;
-}
-b2Math.Random = function () {
-  return Math.random() * 2 - 1;
-}
-b2Math.RandomRange = function (lo, hi) {
-  if (lo === undefined) lo = 0;
-  if (hi === undefined) hi = 0;
-  var r = Math.random();
-  r = (hi - lo) * r + lo;
-  return r;
-}
-b2Math.NextPowerOfTwo = function (x) {
-  if (x === undefined) x = 0;
-  x |= (x >> 1) & 0x7FFFFFFF;
-  x |= (x >> 2) & 0x3FFFFFFF;
-  x |= (x >> 4) & 0x0FFFFFFF;
-  x |= (x >> 8) & 0x00FFFFFF;
-  x |= (x >> 16) & 0x0000FFFF;
-  return x + 1;
-}
-b2Math.IsPowerOfTwo = function (x) {
-  if (x === undefined) x = 0;
-  var result = x > 0 && (x & (x - 1)) == 0;
-  return result;
-}
+
+var b2Math =
+Box2D.Common.Math.b2Math = {
+  Dot: function(a, b) { return a.x * b.x + a.y * b.y; },
+  CrossVV: function(a, b) { return a.x * b.y - a.y * b.x; },
+  CrossVF: function(a, s) { return new b2Vec2(s * a.y, (-s * a.x)); },
+  CrossFV: function(s, a) { return new b2Vec2((-s * a.y), s * a.x); },
+  MulMV: function(A, v) {
+    return new b2Vec2(A.col1.x * v.x + A.col2.x * v.y,
+                      A.col1.y * v.x + A.col2.y * v.y);
+  },
+  MulTMV: function(A, v) {
+    return new b2Vec2(b2Math.Dot(v, A.col1), b2Math.Dot(v, A.col2));
+  },
+  MulX: function(T, v) {
+    var a = b2Math.MulMV(T.R, v);
+    a.x += T.position.x;
+    a.y += T.position.y;
+    return a;
+  },
+  MulXT: function(T, v) {
+    var a = b2Math.SubtractVV(v, T.position);
+    var tX = (a.x * T.R.col1.x + a.y * T.R.col1.y);
+    a.y = (a.x * T.R.col2.x + a.y * T.R.col2.y);
+    a.x = tX;
+    return a;
+  },
+  AddVV: function(a, b) { return new b2Vec2(a.x + b.x, a.y + b.y); },
+  SubtractVV: function(a, b) { return new b2Vec2(a.x - b.x, a.y - b.y); },
+  Distance: function(a, b) {
+    var cX = a.x - b.x;
+    var cY = a.y - b.y;
+    return Math.sqrt(cX * cX + cY * cY);
+  },
+  DistanceSquared: function(a, b) {
+    var cX = a.x - b.x;
+    var cY = a.y - b.y;
+    return (cX * cX + cY * cY);
+  },
+  MulFV: function(s, a) { return new b2Vec2(s * a.x, s * a.y); },
+  AddMM: function(A, B) {
+    return b2Mat22.FromVV(b2Math.AddVV(A.col1, B.col1),
+                          b2Math.AddVV(A.col2, B.col2));
+  },
+  MulMM: function(A, B) {
+    return b2Mat22.FromVV(b2Math.MulMV(A, B.col1), b2Math.MulMV(A, B.col2));
+  },
+  MulTMM: function(A, B) {
+    var c1 = new b2Vec2(b2Math.Dot(A.col1, B.col1), b2Math.Dot(A.col2, B.col1));
+    var c2 = new b2Vec2(b2Math.Dot(A.col1, B.col2), b2Math.Dot(A.col2, B.col2));
+    var C = b2Mat22.FromVV(c1, c2);
+    return C;
+  },
+  AbsV: function(a) { return new b2Vec2(Math.abs(a.x), Math.abs(a.y)); },
+  AbsM: function(A) {
+    return b2Mat22.FromVV(b2Math.AbsV(A.col1), b2Math.AbsV(A.col2));
+  },
+  MinV: function(a, b) {
+    return new b2Vec2(Math.min(a.x, b.x), Math.min(a.y, b.y));
+  },
+  MaxV: function(a, b) {
+    var c = new b2Vec2(Math.max(a.x, b.x), Math.max(a.y, b.y));
+    return c;
+  },
+  Clamp: function(a, low, high) {
+    return a < low ? low : a > high ? high : a;
+  },
+  ClampV: function(a, low, high) {
+    return b2Math.MaxV(low, b2Math.MinV(a, high));
+  },
+  Swap: function(a, b) {
+    var tmp = a[0];
+    a[0] = b[0];
+    b[0] = tmp;
+  },
+  Random: function () { return Math.random() * 2 - 1; },
+  /*
+  // Unused?
+  RandomRange = function (lo, hi) {
+    if (lo === undefined) lo = 0;
+    if (hi === undefined) hi = 0;
+    var r = Math.random();
+    r = (hi - lo) * r + lo;
+    return r;
+  },
+  */
+  NextPowerOfTwo: function (x) {
+    if (x === undefined) x = 0;
+    x |= (x >> 1) & 0x7FFFFFFF;
+    x |= (x >> 2) & 0x3FFFFFFF;
+    x |= (x >> 4) & 0x0FFFFFFF;
+    x |= (x >> 8) & 0x00FFFFFF;
+    x |= (x >> 16) & 0x0000FFFF;
+    return x + 1;
+  },
+  IsPowerOfTwo: function (x) {
+    var result = x > 0 && (x & (x - 1)) == 0;
+    return result;
+  }
+};
+
 Box2D.postDefs.push(function () {
   Box2D.Common.Math.b2Math.b2Vec2_zero = new b2Vec2(0, 0);
   Box2D.Common.Math.b2Math.b2Mat22_identity = b2Mat22.FromVV(new b2Vec2(1, 0), new b2Vec2(0, 1));
