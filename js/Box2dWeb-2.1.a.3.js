1f59de02ed1e9a0cd0f5ae2e3782905dcc072bcd
Alex Russell <slightlyoff@chromium.org>
2014-03-15 21:15:30 -0700

    More math to new OO.

--- js/Box2dWeb-2.1.a.3.js
+++ js/Box2dWeb-2.1.a.3.js
@@ -226,6 +226,55 @@ Box2D.Common.Math.b2Math = {
   }
 };
 
+var b2Sweep =
+Box2D.Common.Math.b2Sweep = Box2D.inherit_({
+  initialize: function() {
+    this.localCenter = new b2Vec2();
+    this.c0 = new b2Vec2;
+    this.c = new b2Vec2();
+    this.a = 0;
+    this.t0 = 0;
+  },
+
+  Set: function(other) {
+    this.localCenter.SetV(other.localCenter);
+    this.c0.SetV(other.c0);
+    this.c.SetV(other.c);
+    this.a0 = other.a0;
+    this.a = other.a;
+    this.t0 = other.t0;
+  },
+  Copy: function() {
+    var copy = new b2Sweep();
+    copy.localCenter.SetV(this.localCenter);
+    copy.c0.SetV(this.c0);
+    copy.c.SetV(this.c);
+    copy.a0 = this.a0;
+    copy.a = this.a;
+    copy.t0 = this.t0;
+    return copy;
+  },
+  GetTransform: function (xf, alpha) {
+    if (alpha === undefined) alpha = 0;
+    xf.position.x = (1 - alpha) * this.c0.x + alpha * this.c.x;
+    xf.position.y = (1 - alpha) * this.c0.y + alpha * this.c.y;
+    var angle = (1 - alpha) * this.a0 + alpha * this.a;
+    xf.R.Set(angle);
+    var tMat = xf.R;
+    xf.position.x -= (tMat.col1.x * this.localCenter.x + tMat.col2.x * this.localCenter.y);
+    xf.position.y -= (tMat.col1.y * this.localCenter.x + tMat.col2.y * this.localCenter.y);
+  },
+  Advance: function (t) {
+    if (t === undefined) t = 0;
+    if (this.t0 < t && 1 - this.t0 > Number.MIN_VALUE) {
+      var alpha = (t - this.t0) / (1 - this.t0);
+      this.c0.x = (1 - alpha) * this.c0.x + alpha * this.c.x;
+      this.c0.y = (1 - alpha) * this.c0.y + alpha * this.c.y;
+      this.a0 = (1 - alpha) * this.a0 + alpha * this.a;
+      this.t0 = t;
+    }
+  }
+});
 
 var b2AABB =
 Box2D.Collision.b2AABB = Box2D.inherit_({
@@ -3228,18 +3277,88 @@ b2Mat22.FromVV = function(c1, c2) {
 };
 
 
-// TODO(slightlyoff): inherit_()
-function b2Mat33() {
-   b2Mat33.b2Mat33.apply(this, arguments);
-   if (this.constructor === b2Mat33) this.b2Mat33.apply(this, arguments);
-};
-Box2D.Common.Math.b2Mat33 = b2Mat33;
+var b2Mat33 =
+Box2D.Common.Math.b2Mat33 = Box2D.inherit_({
+  initialize: function(c1, c2, c3) {
+    // FIXME: move to Array or ByteArray?
+    this.col1 = new b2Vec3();
+    this.col2 = new b2Vec3();
+    this.col3 = new b2Vec3();
+    if (c1 && c2 && c3) {
+      this.SetVVV(c1, c2, c3)
+    }
+  },
+  SetVVV: function(c1, c2, c3) {
+    this.col1.SetV(c1);
+    this.col2.SetV(c2);
+    this.col3.SetV(c3);
+  },
+  Copy: function() {
+    return new b2Mat33(this.col1, this.col2, this.col3);
+  },
+  SetM: function(m) {
+    this.col1.SetV(m.col1);
+    this.col2.SetV(m.col2);
+    this.col3.SetV(m.col3);
+  },
+  AddM: function(m) {
+    this.col1.x += m.col1.x;
+    this.col1.y += m.col1.y;
+    this.col1.z += m.col1.z;
+    this.col2.x += m.col2.x;
+    this.col2.y += m.col2.y;
+    this.col2.z += m.col2.z;
+    this.col3.x += m.col3.x;
+    this.col3.y += m.col3.y;
+    this.col3.z += m.col3.z;
+  },
+  SetIdentity: function() {
+    this.col1.x = this.col2.y = this.col3.z = 1;
 
-// TODO(slightlyoff): inherit_()
-function b2Sweep() {
-   b2Sweep.b2Sweep.apply(this, arguments);
-};
-Box2D.Common.Math.b2Sweep = b2Sweep;
+    this.col2.x = this.col3.x = this.col1.y =
+    this.col3.y = this.col1.z = this.col2.z = 0;
+  },
+  SetZero: function() {
+    this.col1.x = 0;
+    this.col2.x = 0;
+    this.col3.x = 0;
+    this.col1.y = 0;
+    this.col2.y = 0;
+    this.col3.y = 0;
+    this.col1.z = 0;
+    this.col2.z = 0;
+    this.col3.z = 0;
+  },
+  Solve22: function(out, bX, bY) {
+    var a11 = this.col1.x;
+    var a12 = this.col2.x;
+    var a21 = this.col1.y;
+    var a22 = this.col2.y;
+    var det = a11 * a22 - a12 * a21;
+    if (det != 0) { det = 1 / det; }
+    out.x = det * (a22 * bX - a12 * bY);
+    out.y = det * (a11 * bY - a21 * bX);
+    return out;
+  },
+  Solve33: function(out, bX, bY, bZ) {
+    var a11 = this.col1.x;
+    var a21 = this.col1.y;
+    var a31 = this.col1.z;
+    var a12 = this.col2.x;
+    var a22 = this.col2.y;
+    var a32 = this.col2.z;
+    var a13 = this.col3.x;
+    var a23 = this.col3.y;
+    var a33 = this.col3.z;
+    var det = a11 * (a22 * a33 - a32 * a23) + a21 * (a32 * a13 - a12 * a33) + a31 * (a12 * a23 - a22 * a13);
+    if (det != 0) { det = 1 / det; }
+    out.x = det * (bX * (a22 * a33 - a32 * a23) + bY * (a32 * a13 - a12 * a33) + bZ * (a12 * a23 - a22 * a13));
+    out.y = det * (a11 * (bY * a33 - bZ * a23) + a21 * (bZ * a13 - bX * a33) + a31 * (bX * a23 - bY * a13));
+    out.z = det * (a11 * (a22 * bZ - a32 * bY) + a21 * (a32 * bX - a12 * bZ) + a31 * (a12 * bY - a22 * bX));
+    return out;
+  },
+});
+
 
 // TODO(slightlyoff): inherit_()
 function b2Transform() {
@@ -3773,152 +3892,13 @@ Box2D.postDefs.push(function () {
 });
 
 
-b2Mat33.b2Mat33 = function () {
-  this.col1 = new b2Vec3();
-  this.col2 = new b2Vec3();
-  this.col3 = new b2Vec3();
-};
-b2Mat33.prototype.b2Mat33 = function (c1, c2, c3) {
-  if (c1 === undefined) c1 = null;
-  if (c2 === undefined) c2 = null;
-  if (c3 === undefined) c3 = null;
-  if (!c1 && !c2 && !c3) {
-    this.col1.SetZero();
-    this.col2.SetZero();
-    this.col3.SetZero();
-  } else {
-    this.col1.SetV(c1);
-    this.col2.SetV(c2);
-    this.col3.SetV(c3);
-  }
-}
-b2Mat33.prototype.SetVVV = function (c1, c2, c3) {
-  this.col1.SetV(c1);
-  this.col2.SetV(c2);
-  this.col3.SetV(c3);
-}
-b2Mat33.prototype.Copy = function () {
-  return new b2Mat33(this.col1, this.col2, this.col3);
-}
-b2Mat33.prototype.SetM = function (m) {
-  this.col1.SetV(m.col1);
-  this.col2.SetV(m.col2);
-  this.col3.SetV(m.col3);
-}
-b2Mat33.prototype.AddM = function (m) {
-  this.col1.x += m.col1.x;
-  this.col1.y += m.col1.y;
-  this.col1.z += m.col1.z;
-  this.col2.x += m.col2.x;
-  this.col2.y += m.col2.y;
-  this.col2.z += m.col2.z;
-  this.col3.x += m.col3.x;
-  this.col3.y += m.col3.y;
-  this.col3.z += m.col3.z;
-}
-b2Mat33.prototype.SetIdentity = function () {
-  this.col1.x = 1;
-  this.col2.x = 0;
-  this.col3.x = 0;
-  this.col1.y = 0;
-  this.col2.y = 1;
-  this.col3.y = 0;
-  this.col1.z = 0;
-  this.col2.z = 0;
-  this.col3.z = 1;
-}
-b2Mat33.prototype.SetZero = function () {
-  this.col1.x = 0;
-  this.col2.x = 0;
-  this.col3.x = 0;
-  this.col1.y = 0;
-  this.col2.y = 0;
-  this.col3.y = 0;
-  this.col1.z = 0;
-  this.col2.z = 0;
-  this.col3.z = 0;
-}
-b2Mat33.prototype.Solve22 = function (out, bX, bY) {
-  var a11 = this.col1.x;
-  var a12 = this.col2.x;
-  var a21 = this.col1.y;
-  var a22 = this.col2.y;
-  var det = a11 * a22 - a12 * a21;
-  if (det != 0) {
-    det = 1 / det;
-  }
-  out.x = det * (a22 * bX - a12 * bY);
-  out.y = det * (a11 * bY - a21 * bX);
-  return out;
-}
-b2Mat33.prototype.Solve33 = function (out, bX, bY, bZ) {
-  var a11 = this.col1.x;
-  var a21 = this.col1.y;
-  var a31 = this.col1.z;
-  var a12 = this.col2.x;
-  var a22 = this.col2.y;
-  var a32 = this.col2.z;
-  var a13 = this.col3.x;
-  var a23 = this.col3.y;
-  var a33 = this.col3.z;
-  var det = a11 * (a22 * a33 - a32 * a23) + a21 * (a32 * a13 - a12 * a33) + a31 * (a12 * a23 - a22 * a13);
-  if (det != 0) {
-    det = 1 / det;
-  }
-  out.x = det * (bX * (a22 * a33 - a32 * a23) + bY * (a32 * a13 - a12 * a33) + bZ * (a12 * a23 - a22 * a13));
-  out.y = det * (a11 * (bY * a33 - bZ * a23) + a21 * (bZ * a13 - bX * a33) + a31 * (bX * a23 - bY * a13));
-  out.z = det * (a11 * (a22 * bZ - a32 * bY) + a21 * (a32 * bX - a12 * bZ) + a31 * (a12 * bY - a22 * bX));
-  return out;
-}
 
 Box2D.postDefs.push(function () {
   Box2D.Common.Math.b2Math.b2Vec2_zero = new b2Vec2(0, 0);
   Box2D.Common.Math.b2Math.b2Mat22_identity = b2Mat22.FromVV(new b2Vec2(1, 0), new b2Vec2(0, 1));
   Box2D.Common.Math.b2Math.b2Transform_identity = new b2Transform(b2Math.b2Vec2_zero, b2Math.b2Mat22_identity);
 });
-b2Sweep.b2Sweep = function () {
-  this.localCenter = new b2Vec2();
-  this.c0 = new b2Vec2;
-  this.c = new b2Vec2();
-};
-b2Sweep.prototype.Set = function (other) {
-  this.localCenter.SetV(other.localCenter);
-  this.c0.SetV(other.c0);
-  this.c.SetV(other.c);
-  this.a0 = other.a0;
-  this.a = other.a;
-  this.t0 = other.t0;
-}
-b2Sweep.prototype.Copy = function () {
-  var copy = new b2Sweep();
-  copy.localCenter.SetV(this.localCenter);
-  copy.c0.SetV(this.c0);
-  copy.c.SetV(this.c);
-  copy.a0 = this.a0;
-  copy.a = this.a;
-  copy.t0 = this.t0;
-  return copy;
-}
-b2Sweep.prototype.GetTransform = function (xf, alpha) {
-  if (alpha === undefined) alpha = 0;
-  xf.position.x = (1 - alpha) * this.c0.x + alpha * this.c.x;
-  xf.position.y = (1 - alpha) * this.c0.y + alpha * this.c.y;
-  var angle = (1 - alpha) * this.a0 + alpha * this.a;
-  xf.R.Set(angle);
-  var tMat = xf.R;
-  xf.position.x -= (tMat.col1.x * this.localCenter.x + tMat.col2.x * this.localCenter.y);
-  xf.position.y -= (tMat.col1.y * this.localCenter.x + tMat.col2.y * this.localCenter.y);
-}
-b2Sweep.prototype.Advance = function (t) {
-  if (t === undefined) t = 0;
-  if (this.t0 < t && 1 - this.t0 > Number.MIN_VALUE) {
-    var alpha = (t - this.t0) / (1 - this.t0);
-    this.c0.x = (1 - alpha) * this.c0.x + alpha * this.c.x;
-    this.c0.y = (1 - alpha) * this.c0.y + alpha * this.c.y;
-    this.a0 = (1 - alpha) * this.a0 + alpha * this.a;
-    this.t0 = t;
-  }
-}
+
 b2Transform.b2Transform = function () {
   this.position = new b2Vec2;
   this.R = new b2Mat22();
