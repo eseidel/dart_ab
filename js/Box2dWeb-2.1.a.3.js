77fd1b8568bb53accce47bd28f4f4f3133fc6405
Alex Russell <slightlyoff@chromium.org>
2014-03-16 22:01:12 -0700

    Refactor checkpoint.

--- js/Box2dWeb-2.1.a.3.js
+++ js/Box2dWeb-2.1.a.3.js
@@ -6481,7 +6481,6 @@ var b2BuoyancyController =
 Box2D.Dynamics.Controllers.b2BuoyancyController = Box2D.inherit_({
   extends: b2Controller,
   initialize: function() {
-    b2Controller.apply(this, arguments);
     this.normal = new b2Vec2(0, (-1));
     this.offset = 0;
     this.density = 0;
@@ -6550,43 +6549,190 @@ Box2D.Dynamics.Controllers.b2BuoyancyController = Box2D.inherit_({
   },
 });
 
+var b2ConstantAccelController =
+Box2D.Dynamics.Controllers.b2ConstantAccelController = Box2D.inherit_({
+  extends: b2Controller,
+  initialize: function() {
+    this.A = new b2Vec2(0, 0);
+  },
+  Step: function (step) {
+    var smallA = new b2Vec2(this.A.x * step.dt, this.A.y * step.dt);
+    for (var i = this.m_bodyList; i; i = i.nextBody) {
+      var body = i.body;
+      if (!body.IsAwake()) continue;
+      body.SetLinearVelocity(
+        new b2Vec2(
+          body.GetLinearVelocity().x + smallA.x,
+          body.GetLinearVelocity().y + smallA.y
+        )
+      );
+    }
+  },
+});
+
+var b2Controller =
+Box2D.Dynamics.Controllers.b2Controller = Box2D.inherit_({
+  Step: function (step) {},
+  Draw: function (debugDraw) {},
+  AddBody: function (body) {
+    var edge = new b2ControllerEdge();
+    edge.controller = this;
+    edge.body = body;
+    edge.nextBody = this.m_bodyList;
+    edge.prevBody = null;
+    this.m_bodyList = edge;
+    if (edge.nextBody) edge.nextBody.prevBody = edge;
+    this.m_bodyCount++;
+    edge.nextController = body.m_controllerList;
+    edge.prevController = null;
+    body.m_controllerList = edge;
+    if (edge.nextController) edge.nextController.prevController = edge;
+    body.m_controllerCount++;
+  },
+  RemoveBody: function (body) {
+    var edge = body.m_controllerList;
+    while (edge && edge.controller != this)
+    edge = edge.nextController;
+    if (edge.prevBody) edge.prevBody.nextBody = edge.nextBody;
+    if (edge.nextBody) edge.nextBody.prevBody = edge.prevBody;
+    if (edge.nextController) edge.nextController.prevController = edge.prevController;
+    if (edge.prevController) edge.prevController.nextController = edge.nextController;
+    if (this.m_bodyList == edge) this.m_bodyList = edge.nextBody;
+    if (body.m_controllerList == edge) body.m_controllerList = edge.nextController;
+    body.m_controllerCount--;
+    this.m_bodyCount--;
+  },
+  Clear: function () {
+    while (this.m_bodyList)
+    this.RemoveBody(this.m_bodyList.body);
+  },
+  GetNext: function () {
+    return this.m_next;
+  },
+  GetWorld: function () {
+    return this.m_world;
+  },
+  GetBodyList: function () {
+    return this.m_bodyList;
+  },
+});
+
+var b2ConstantForceController =
+Box2D.Dynamics.Controllers.b2ConstantForceController = Box2D.inherit_({
+  extends: b2Controller,
+  initialize: function() {
+    this.F = new b2Vec2(0, 0);
+  },
+  Step: function (step) {
+    for (var i = this.m_bodyList; i; i = i.nextBody) {
+      var body = i.body;
+      if (!body.IsAwake()) continue;
+      body.ApplyForce(this.F, body.GetWorldCenter());
+    }
+  },
+});
+
+var b2ControllerEdge =
+Box2D.Dynamics.Controllers.b2ControllerEdge = function() {};
+
+var b2GravityController =
+Box2D.Dynamics.Controllers.b2GravityController = Box2D.inherit_({
+  extends: b2Controller,
+  initialize: function() {
+    this.G = 1;
+    this.invSqr = true;
+  },
+  Step: function (step) {
+    var i = null;
+    var body1 = null;
+    var p1 = null;
+    var mass1 = 0;
+    var j = null;
+    var body2 = null;
+    var p2 = null;
+    var dx = 0;
+    var dy = 0;
+    var r2 = 0;
+    var f = null;
+    if (this.invSqr) {
+      for (i = this.m_bodyList; i; i = i.nextBody) {
+        body1 = i.body;
+        p1 = body1.GetWorldCenter();
+        mass1 = body1.GetMass();
+        for (j = this.m_bodyList; j != i; j = j.nextBody) {
+          body2 = j.body;
+          p2 = body2.GetWorldCenter();
+          dx = p2.x - p1.x;
+          dy = p2.y - p1.y;
+          r2 = dx * dx + dy * dy;
+          if (r2 < Number.MIN_VALUE) continue;
+          f = new b2Vec2(dx, dy);
+          f.Multiply(this.G / r2 / Math.sqrt(r2) * mass1 * body2.GetMass());
+          if (body1.IsAwake()) body1.ApplyForce(f, p1);
+          f.Multiply((-1));
+          if (body2.IsAwake()) body2.ApplyForce(f, p2);
+        }
+      }
+    } else {
+      for (i = this.m_bodyList; i; i = i.nextBody) {
+        body1 = i.body;
+        p1 = body1.GetWorldCenter();
+        mass1 = body1.GetMass();
+        for (j = this.m_bodyList; j != i; j = j.nextBody) {
+          body2 = j.body;
+          p2 = body2.GetWorldCenter();
+          dx = p2.x - p1.x;
+          dy = p2.y - p1.y;
+          r2 = dx * dx + dy * dy;
+          if (r2 < Number.MIN_VALUE) continue;
+          f = new b2Vec2(dx, dy);
+          f.Multiply(this.G / r2 * mass1 * body2.GetMass());
+          if (body1.IsAwake()) body1.ApplyForce(f, p1);
+          f.Multiply((-1));
+          if (body2.IsAwake()) body2.ApplyForce(f, p2);
+        }
+      }
+    }
+  },
+});
+
+var b2TensorDampingController =
+Box2D.Dynamics.Controllers.b2TensorDampingController = Box2D.inherit_({
+  extends: b2Controller,
+  initialize: function() {
+    this.T = new b2Mat22();
+    this.maxTimestep = 0;
+  },
+  SetAxisAligned: function (xDamping, yDamping) {
+    if (xDamping === undefined) xDamping = 0;
+    if (yDamping === undefined) yDamping = 0;
+    this.T.col1.x = (-xDamping);
+    this.T.col1.y = 0;
+    this.T.col2.x = 0;
+    this.T.col2.y = (-yDamping);
+    if (xDamping > 0 || yDamping > 0) {
+      this.maxTimestep = 1 / Math.max(xDamping, yDamping);
+    } else {
+      this.maxTimestep = 0;
+    }
+  },
+  Step: function (step) {
+    var timestep = step.dt;
+    if (timestep <= Number.MIN_VALUE) return;
+    if (timestep > this.maxTimestep && this.maxTimestep > 0) timestep = this.maxTimestep;
+    for (var i = this.m_bodyList; i; i = i.nextBody) {
+      var body = i.body;
+      if (!body.IsAwake()) {
+        continue;
+      }
+      var damping = body.GetWorldVector(b2Math.MulMV(this.T, body.GetLocalVector(body.GetLinearVelocity())));
+      body.SetLinearVelocity(new b2Vec2(body.GetLinearVelocity().x + damping.x * timestep, body.GetLinearVelocity().y + damping.y * timestep));
+    }
+  },
+});
 
 
-// TODO(slightlyoff): inherit_()
-function b2ConstantAccelController() {
-   b2ConstantAccelController.b2ConstantAccelController.apply(this, arguments);
-};
-Box2D.Dynamics.Controllers.b2ConstantAccelController = b2ConstantAccelController;
 
-// TODO(slightlyoff): inherit_()
-function b2ConstantForceController() {
-   b2ConstantForceController.b2ConstantForceController.apply(this, arguments);
-};
-Box2D.Dynamics.Controllers.b2ConstantForceController = b2ConstantForceController;
-
-// TODO(slightlyoff): inherit_()
-function b2Controller() {
-   b2Controller.b2Controller.apply(this, arguments);
-};
-Box2D.Dynamics.Controllers.b2Controller = b2Controller;
-
-// TODO(slightlyoff): inherit_()
-function b2ControllerEdge() {
-   b2ControllerEdge.b2ControllerEdge.apply(this, arguments);
-};
-Box2D.Dynamics.Controllers.b2ControllerEdge = b2ControllerEdge;
-
-// TODO(slightlyoff): inherit_()
-function b2GravityController() {
-   b2GravityController.b2GravityController.apply(this, arguments);
-};
-Box2D.Dynamics.Controllers.b2GravityController = b2GravityController;
-
-// TODO(slightlyoff): inherit_()
-function b2TensorDampingController() {
-   b2TensorDampingController.b2TensorDampingController.apply(this, arguments);
-};
-Box2D.Dynamics.Controllers.b2TensorDampingController = b2TensorDampingController;
 
 // TODO(slightlyoff): inherit_()
 function b2DistanceJoint() {
@@ -6800,174 +6946,6 @@ b2DebugDraw.prototype.DrawSegment = function (p1, p2, color) {}
 b2DebugDraw.prototype.DrawTransform = function (xf) {}
 
 
-Box2D.inherit(b2ConstantAccelController, Box2D.Dynamics.Controllers.b2Controller);
-b2ConstantAccelController.prototype.__super = Box2D.Dynamics.Controllers.b2Controller.prototype;
-b2ConstantAccelController.b2ConstantAccelController = function () {
-  Box2D.Dynamics.Controllers.b2Controller.b2Controller.apply(this, arguments);
-  this.A = new b2Vec2(0, 0);
-};
-b2ConstantAccelController.prototype.Step = function (step) {
-  var smallA = new b2Vec2(this.A.x * step.dt, this.A.y * step.dt);
-  for (var i = this.m_bodyList; i; i = i.nextBody) {
-    var body = i.body;
-    if (!body.IsAwake()) continue;
-    body.SetLinearVelocity(new b2Vec2(body.GetLinearVelocity().x + smallA.x, body.GetLinearVelocity().y + smallA.y));
-  }
-}
-Box2D.inherit(b2ConstantForceController, Box2D.Dynamics.Controllers.b2Controller);
-b2ConstantForceController.prototype.__super = Box2D.Dynamics.Controllers.b2Controller.prototype;
-b2ConstantForceController.b2ConstantForceController = function () {
-  Box2D.Dynamics.Controllers.b2Controller.b2Controller.apply(this, arguments);
-  this.F = new b2Vec2(0, 0);
-};
-b2ConstantForceController.prototype.Step = function (step) {
-  for (var i = this.m_bodyList; i; i = i.nextBody) {
-    var body = i.body;
-    if (!body.IsAwake()) continue;
-    body.ApplyForce(this.F, body.GetWorldCenter());
-  }
-}
-b2Controller.b2Controller = function () {};
-b2Controller.prototype.Step = function (step) {}
-b2Controller.prototype.Draw = function (debugDraw) {}
-b2Controller.prototype.AddBody = function (body) {
-  var edge = new b2ControllerEdge();
-  edge.controller = this;
-  edge.body = body;
-  edge.nextBody = this.m_bodyList;
-  edge.prevBody = null;
-  this.m_bodyList = edge;
-  if (edge.nextBody) edge.nextBody.prevBody = edge;
-  this.m_bodyCount++;
-  edge.nextController = body.m_controllerList;
-  edge.prevController = null;
-  body.m_controllerList = edge;
-  if (edge.nextController) edge.nextController.prevController = edge;
-  body.m_controllerCount++;
-}
-b2Controller.prototype.RemoveBody = function (body) {
-  var edge = body.m_controllerList;
-  while (edge && edge.controller != this)
-  edge = edge.nextController;
-  if (edge.prevBody) edge.prevBody.nextBody = edge.nextBody;
-  if (edge.nextBody) edge.nextBody.prevBody = edge.prevBody;
-  if (edge.nextController) edge.nextController.prevController = edge.prevController;
-  if (edge.prevController) edge.prevController.nextController = edge.nextController;
-  if (this.m_bodyList == edge) this.m_bodyList = edge.nextBody;
-  if (body.m_controllerList == edge) body.m_controllerList = edge.nextController;
-  body.m_controllerCount--;
-  this.m_bodyCount--;
-}
-b2Controller.prototype.Clear = function () {
-  while (this.m_bodyList)
-  this.RemoveBody(this.m_bodyList.body);
-}
-b2Controller.prototype.GetNext = function () {
-  return this.m_next;
-}
-b2Controller.prototype.GetWorld = function () {
-  return this.m_world;
-}
-b2Controller.prototype.GetBodyList = function () {
-  return this.m_bodyList;
-}
-b2ControllerEdge.b2ControllerEdge = function () {};
-Box2D.inherit(b2GravityController, Box2D.Dynamics.Controllers.b2Controller);
-b2GravityController.prototype.__super = Box2D.Dynamics.Controllers.b2Controller.prototype;
-b2GravityController.b2GravityController = function () {
-  Box2D.Dynamics.Controllers.b2Controller.b2Controller.apply(this, arguments);
-  this.G = 1;
-  this.invSqr = true;
-};
-b2GravityController.prototype.Step = function (step) {
-  var i = null;
-  var body1 = null;
-  var p1 = null;
-  var mass1 = 0;
-  var j = null;
-  var body2 = null;
-  var p2 = null;
-  var dx = 0;
-  var dy = 0;
-  var r2 = 0;
-  var f = null;
-  if (this.invSqr) {
-    for (i = this.m_bodyList;
-    i; i = i.nextBody) {
-      body1 = i.body;
-      p1 = body1.GetWorldCenter();
-      mass1 = body1.GetMass();
-      for (j = this.m_bodyList;
-      j != i; j = j.nextBody) {
-        body2 = j.body;
-        p2 = body2.GetWorldCenter();
-        dx = p2.x - p1.x;
-        dy = p2.y - p1.y;
-        r2 = dx * dx + dy * dy;
-        if (r2 < Number.MIN_VALUE) continue;
-        f = new b2Vec2(dx, dy);
-        f.Multiply(this.G / r2 / Math.sqrt(r2) * mass1 * body2.GetMass());
-        if (body1.IsAwake()) body1.ApplyForce(f, p1);
-        f.Multiply((-1));
-        if (body2.IsAwake()) body2.ApplyForce(f, p2);
-      }
-    }
-  } else {
-    for (i = this.m_bodyList;
-    i; i = i.nextBody) {
-      body1 = i.body;
-      p1 = body1.GetWorldCenter();
-      mass1 = body1.GetMass();
-      for (j = this.m_bodyList;
-      j != i; j = j.nextBody) {
-        body2 = j.body;
-        p2 = body2.GetWorldCenter();
-        dx = p2.x - p1.x;
-        dy = p2.y - p1.y;
-        r2 = dx * dx + dy * dy;
-        if (r2 < Number.MIN_VALUE) continue;
-        f = new b2Vec2(dx, dy);
-        f.Multiply(this.G / r2 * mass1 * body2.GetMass());
-        if (body1.IsAwake()) body1.ApplyForce(f, p1);
-        f.Multiply((-1));
-        if (body2.IsAwake()) body2.ApplyForce(f, p2);
-      }
-    }
-  }
-}
-Box2D.inherit(b2TensorDampingController, Box2D.Dynamics.Controllers.b2Controller);
-b2TensorDampingController.prototype.__super = Box2D.Dynamics.Controllers.b2Controller.prototype;
-b2TensorDampingController.b2TensorDampingController = function () {
-  Box2D.Dynamics.Controllers.b2Controller.b2Controller.apply(this, arguments);
-  this.T = new b2Mat22();
-  this.maxTimestep = 0;
-};
-b2TensorDampingController.prototype.SetAxisAligned = function (xDamping, yDamping) {
-  if (xDamping === undefined) xDamping = 0;
-  if (yDamping === undefined) yDamping = 0;
-  this.T.col1.x = (-xDamping);
-  this.T.col1.y = 0;
-  this.T.col2.x = 0;
-  this.T.col2.y = (-yDamping);
-  if (xDamping > 0 || yDamping > 0) {
-    this.maxTimestep = 1 / Math.max(xDamping, yDamping);
-  } else {
-    this.maxTimestep = 0;
-  }
-}
-b2TensorDampingController.prototype.Step = function (step) {
-  var timestep = step.dt;
-  if (timestep <= Number.MIN_VALUE) return;
-  if (timestep > this.maxTimestep && this.maxTimestep > 0) timestep = this.maxTimestep;
-  for (var i = this.m_bodyList; i; i = i.nextBody) {
-    var body = i.body;
-    if (!body.IsAwake()) {
-      continue;
-    }
-    var damping = body.GetWorldVector(b2Math.MulMV(this.T, body.GetLocalVector(body.GetLinearVelocity())));
-    body.SetLinearVelocity(new b2Vec2(body.GetLinearVelocity().x + damping.x * timestep, body.GetLinearVelocity().y + damping.y * timestep));
-  }
-}
 
 Box2D.inherit(b2DistanceJoint, Box2D.Dynamics.Joints.b2Joint);
 b2DistanceJoint.prototype.__super = Box2D.Dynamics.Joints.b2Joint.prototype;
